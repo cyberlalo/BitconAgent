0a48f44db71e92e16dfed48781064fd56937e1e8
cyberlalo <laloarauxo@gmail.com>
2026-02-03 22:52:15 +0000

    Complete Bitcoin Analysis Agent

--- streamlit_app.py
+++ streamlit_app.py
@@ -372,7 +372,7 @@ with indicator_tabs[0]:
             title="Oscilador Estocástico",
             xaxis_title="Data",
             yaxis_title="Valor (%)",
-            yaxis=dict(range=[-160, 160]),
+            yaxis=dict(range=[-180, 180]),
             hovermode='x unified',
             height=400
         )
